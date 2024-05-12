205cd6e3068befe74aa9b7197676e7e17a8577a0
George Bartlett <george@graide.co.uk>
2024-05-12 23:44:23 +0100

    adds eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs['recommended-flat'],
+]
